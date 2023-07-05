dd2736d2f8a9aee48e12d0a0462427a3fb8b26e1
schrattenholz <46528228+schrattenholz@users.noreply.github.com>
2023-07-05 18:12:24 +0200

    Update OrderSale_OrderExtension.php

--- src/OrderSale_OrderExtension.php
+++ src/OrderSale_OrderExtension.php
@@ -628,7 +628,7 @@ Injector::inst()->get(LoggerInterface::class)->error('addProduct----------------
 			Injector::inst()->get(LoggerInterface::class)->error('-----------------____-----_____ default-Produkt benutzen');
 			if($product->GroupPreise()->Count()>0){
 				Injector::inst()->get(LoggerInterface::class)->error('-----------------____-----_____ suche default-Produkt mit Bestand');
-				$variantID=$product->GroupPreise()->Sort('SortID','ASC')->Filter(["Inventory:GreaterThan"=>"0"])->First()->ID;
+				$variantID=$product->GroupPreise()->Sort('SortID','ASC')->First()->ID;
 				$quantities=$this->owner->FreeQuantity(array('id'=>$product->ID,'variant01'=>$variantID,'productID'=>$product->ID));
 				$quantity=$quantities['ClientsQuantity'];
 				$quantity=0;
